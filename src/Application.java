319819e0f2c134ea6467bc1a4658dce912f31bea
makarovv <dayana.hristova98@gmail.com>
2026-04-28 17:25:28 +0200

    add exercise 2 to repo

--- src/Application.java
+++ src/Application.java
@@ -1,3 +1,8 @@
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
+
 public class Application {
     public static void main(String[] args) {
         Display display[] = new Display[4];
@@ -5,6 +10,15 @@ public class Application {
         display[1] = new Book("To Kill a Mockingbird", "English", 12.99, "Harper Lee", "978-0061120084");
         display[2] = new Car("Red", 200, 1500.0);
 
-        
+        try{
+            FileOutputStream fileOut = new FileOutputStream("test.ser");
+            ObjectOutputStream os = new ObjectOutputStream(fileOut);
+            for (Display d : display) {
+                os.writeObject(d);
+            }
+            os.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
